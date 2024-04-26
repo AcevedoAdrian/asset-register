4bf0a8cfc6de8947328bd4c9508b63ff93b4e1bd
AcevedoAdrian <acevedohugoadrian@gmail.com>
2024-04-26 12:31:53 -0300

    feat: Add path relative for tailwind and pug

--- src/app.js
+++ src/app.js
@@ -1,24 +1,25 @@
-import express from "express";
-import config from "./config/config.js";
-import usersRouter from "./routes/users.routes.js";
+import express from 'express';
+import { publicPath } from './utils/utils.js';
+import config from './config/config.js';
+import usersRouter from './routes/users.routes.js';
 
 // Create express app
 const app = express();
+
+// Static files
+app.use(express.static(publicPath));
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Static files
-app.use(express.static("public"));
-
 // Enable PUG
-app.set("view engine", "pug");
-app.set("views", "./src/views");
+app.set('view engine', 'pug');
+app.set('views', './src/views');
 
-//Routes
-app.use("/auth", usersRouter);
+// Routes
+app.use('/auth', usersRouter);
 
 app.listen(config.PORT, () => {
   console.log(`Server is listening on port ${config.PORT}`);
 });
-
-
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/views/**/*.{html,js,pug}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
